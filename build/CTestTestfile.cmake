fb8f115623ad2262fc9336a2267ea4ff530655e5
JiatongSun <SJT_sjtu1998@163.com>
2020-06-21 15:18:30 -0400

    fix bug

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/sun/Documents/Project/xarm/src
+# Build directory: /home/sun/Documents/Project/xarm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("xarm_description")
+subdirs("xarm_moveit_config")
+subdirs("xarm_planning")
---
+++ build/xarm_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sun/Documents/Project/xarm/src/xarm_moveit_config
+# Build directory: /home/sun/Documents/Project/xarm/build/xarm_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
